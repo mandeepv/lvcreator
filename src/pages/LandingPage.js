c036af2340033b61c8a256e6121fa502e357e36a
mandeepv <mandeepv98@gmail.com>
2023-08-06 00:03:00 +0530

    cosmetic changes to LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import Sheet from '@mui/joy/Sheet';
 import Typography from '@mui/joy/Typography';
-import NavBar from '../components/NavBar'; // Make sure to import your NavBar component correctly
+import NavBar from '../components/NavBar';
 import Box from '@mui/material/Box';
 import Card from '@mui/joy/Card';
 import HeroPic from '../assets/images/hero-divider-1500w.png'
@@ -10,10 +10,10 @@ import Button from '@mui/joy/Button';
 import { styled } from '@mui/system';  
 
 const CustomButton = styled(Button)({
-    color: '#000',  // make the text black
+    color: '#000',
     cursor: 'pointer',
     display: 'inline-block',
-    padding: '15px 30px',  // increase padding to make the button bigger
+    padding: '15px 30px',
     fontSize: 18,
     boxShadow: '8px 7px 0 0 #000',
     fontStyle: 'normal',
@@ -24,14 +24,11 @@ const CustomButton = styled(Button)({
     borderRadius: 10,
     backgroundColor: '#ffcf77',
 
-    
-
     '&:active': {
-        transform: 'translateY(10px)', // Gives the effect of the button being pressed
-        boxShadow: '0px 0px 0 0 #000', // Reduces shadow to give depth perception
+        transform: 'translateY(10px)', 
+        boxShadow: '0px 0px 0 0 #000', 
     },
 });
-
 
 const LandingPage = () => {
   return (
@@ -85,16 +82,18 @@ const LandingPage = () => {
               alignItems: 'center',
               justifyContent: 'center',
               color: '#fff',
+              overflow: 'hidden',
+              padding: 0,
             }}
-            >
+          >
             <Typography
               level="h1"
               component="h1"
               sx={{
                 fontWeight: 'bold',
                 textAlign: 'center',
-                mt: 3, // Added top margin
-                mb: 1, // Added bottom margin
+                mt: { xs: 5, sm: 8, md: 10 },  // Responsive top margin
+                mb: 1,
               }}
             >
               Everybody is an Influencer
@@ -104,11 +103,12 @@ const LandingPage = () => {
               component="h2"
               sx={{
                 textAlign: 'center',
-                mb: 3, // Added bottom margin
+                mb: 3, // Bottom margin, adjust as needed
               }}
             >
               Unlock your full potential and grow with our marketing platform.
-            </Typography> <Box sx={{ display: 'flex', gap: 2 }}>
+            </Typography>
+            <Box sx={{ display: 'flex', gap: 2, mb: { xs: 2, sm: 2, md: 4 }, mt:2 }}>
               <CustomButton>
                 SIGN ME UP!
               </CustomButton>
@@ -116,13 +116,14 @@ const LandingPage = () => {
                 LOGIN
               </CustomButton>
             </Box>
-            <img
-              src={HeroPic}
-              alt="divider"
-              style={{
+            <Box
+              sx={{
+                height: { xs: '300px', md: '400px', lg: '500px' },
                 width: '100%',
-                maxWidth: '100%',
-                height: 'auto',
+                backgroundImage: `url(${HeroPic})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
               }}
             />
           </Card>
