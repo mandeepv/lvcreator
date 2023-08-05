bc0bb07edf284f39731a6eda71e2cf6274f6d52c
mandeepv <mandeepv98@gmail.com>
2023-08-05 12:54:11 +0530

    Crucial: Responsive HomePage testing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,8 +5,6 @@ import TabList from '@mui/joy/TabList';
 import Tab, { tabClasses } from '@mui/joy/Tab';
 import TabPanel from '@mui/joy/TabPanel';
 import Typography from '@mui/joy/Typography';
-import Input from '@mui/joy/Input';
-import SearchRounded from '@mui/icons-material/SearchRounded';
 import { CssVarsProvider } from '@mui/joy';
 import Sheet from '@mui/joy/Sheet';
 import React from 'react';
@@ -15,26 +13,14 @@ import AppliedCampaignCard from '../components/AppliedCampaignCard';
 import WorkCampaignCard from '../components/WorkCampaignCard';
 import UnderReviewCampaignCard from '../components/UnderReviewCampaignCard';
 import NavBar from '../components/NavBar';
-import Card from '@mui/joy/Card';
-
-import IconButton from '@mui/joy/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
-import Dropdown from '@mui/joy/Dropdown';
-import Menu from '@mui/joy/Menu';
-import MenuButton from '@mui/joy/MenuButton';
-import MenuItem from '@mui/joy/MenuItem';
-import MoreVert from '@mui/icons-material/MoreVert';
-
 
 export default function TabsPageExample() {
   const [index, setIndex] = React.useState(0);
   return (
     <CssVarsProvider>
-      
       <Sheet
         sx={{
-          width: { xs: '100%', md: 500 },
+          width: { xs: '100%', md: '100%', lg: '98%', xl: '100%' },
           mx: 'auto',
           my: 1,
           py: 0,
@@ -44,107 +30,94 @@ export default function TabsPageExample() {
           gap: 2,
           borderRadius: 'sm',
           boxShadow: 'md',
-          backgroundColor: 'transparent',  
+          backgroundColor: 'transparent',
         }}
       >
-    <Box
-      sx={{
-        flexGrow: 1,
-
-        borderRadius: 'md',
-      }}
-    >
-<NavBar showMenuButton/>
-      <Tabs
-        aria-label="Pipeline"
-        value={index}
-        onChange={(event, value) => setIndex(value)}
-      >
-        <TabList
+        <Box
           sx={{
-            pt: 2,
-            justifyContent: 'center',
-            [`&& .${tabClasses.root}`]: {
-              flex: 'initial',
-              bgcolor: 'transparent',
-              '&:hover': {
-                bgcolor: 'transparent',
-              },
-              [`&.${tabClasses.selected}`]: {
-                color: 'primary.plainColor',
-                '&::after': {
-                  height: '3px',
-                  borderTopLeftRadius: '3px',
-                  borderTopRightRadius: '3px',
-                  bgcolor: 'primary.500',
-                },
-              },
-            },
+            flexGrow: 1,
+            borderRadius: 'md',
           }}
         >
-          <Tab indicatorInset>
-            Your Campaigns{' '}
-            <Chip
-              size="sm"
-              variant="soft"    
-              color={index === 0 ? 'primary' : 'neutral'}
-              sx={{ ml: 1 }}
+          <NavBar showMenuButton />
+          <Tabs
+            aria-label="Pipeline"
+            value={index}
+            onChange={(event, value) => setIndex(value)}
+          >
+            <TabList
+              sx={{
+                pt: 2,
+                justifyContent: 'center',
+                [`&& .${tabClasses.root}`]: {
+                  flex: 'initial',
+                  bgcolor: 'transparent',
+                  '&:hover': {
+                    bgcolor: 'transparent',
+                  },
+                  [`&.${tabClasses.selected}`]: {
+                    color: 'primary.plainColor',
+                    '&::after': {
+                      height: '3px',
+                      borderTopLeftRadius: '3px',
+                      borderTopRightRadius: '3px',
+                      bgcolor: 'primary.500',
+                    },
+                  },
+                },
+              }}
             >
-              2
-            </Chip>
-          </Tab>
-          <Tab indicatorInset>
-            New Campaigns{' '}
-            <Chip
-              size="sm"
-              variant="soft"
-              color={index === 1 ? 'primary' : 'neutral'}
-              sx={{ ml: 1 }}
-            >
-              4
-            </Chip>
-          </Tab>
-        </TabList>
-          <TabPanel value={0} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Tab indicatorInset>
+                Your Campaigns{' '}
+                <Chip
+                  size="sm"
+                  variant="soft"
+                  color={index === 0 ? 'primary' : 'neutral'}
+                  sx={{ ml: 1 }}
+                >
+                  2
+                </Chip>
+              </Tab>
+              <Tab indicatorInset>
+                New Campaigns{' '}
+                <Chip
+                  size="sm"
+                  variant="soft"
+                  color={index === 1 ? 'primary' : 'neutral'}
+                  sx={{ ml: 1 }}
+                >
+                  4
+                </Chip>
+              </Tab>
+            </TabList>
+            <TabPanel value={0} sx={{ display: 'flex', flexDirection: { xs: 'column', lg: 'row' }, justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+  <Box sx={{ maxWidth: '500px', width: '100%', marginBottom: 2, display: 'flex', justifyContent: 'center' }}>
+    <WorkCampaignCard />
+  </Box>
+  <Box sx={{ maxWidth: '500px', width: '100%', display: 'flex', justifyContent: 'center' }}>
+    <UnderReviewCampaignCard />
+  </Box>
+</TabPanel>
+<TabPanel value={1} sx={{ display: 'flex', flexDirection: { xs: 'column', lg: 'row' }, justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+  <Box sx={{ maxWidth: '500px', width: '100%', marginBottom: 2, display: 'flex', justifyContent: 'center' }}>
+    <CampaignCard />
+  </Box>
+  <Box sx={{ maxWidth: '500px', width: '100%', marginBottom: 2, display: 'flex', justifyContent: 'center' }}>
+    <CampaignCard />
+  </Box>
+  <Box sx={{ maxWidth: '500px', width: '100%', marginBottom: 2, display: 'flex', justifyContent: 'center' }}>
+    <AppliedCampaignCard />
+  </Box>
+  <Box sx={{ maxWidth: '500px', width: '100%', display: 'flex', justifyContent: 'center' }}>
+    <AppliedCampaignCard />
+  </Box>
+</TabPanel>
 
-            <Typography
-              level="h2"
-              component="div"
-              fontSize="lg"
-              textColor="text.primary"
-              
-            >
-              <WorkCampaignCard/>
-              <br/>
-              <UnderReviewCampaignCard/>
-            </Typography>
-        
 
 
-
-
-          </TabPanel>
-          <TabPanel value={1} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Typography
-              level="h2"
-              component="div"
-              fontSize="lg"
-              textColor="text.primary"
-            >
-              <CampaignCard/>
-              <br/>
-              <CampaignCard/>
-              <br/>
-              <AppliedCampaignCard/>
-              <br/>
-              <AppliedCampaignCard/>
-
-            </Typography>
-          </TabPanel>
-
-      </Tabs>
-    </Box>
-    </Sheet>
+          </Tabs>
+        </Box>
+      </Sheet>
     </CssVarsProvider>
   );
 }
